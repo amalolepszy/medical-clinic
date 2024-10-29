ca568dc63de7361e13ad77463226366fa55473f7
amalolepszy <67199354+amalolepszy@users.noreply.github.com>
2024-10-29 21:11:08 +0100

    3: Extend Patient class and tests (#11)

--- src/main/java/pl/wsb/lab/Clinic.java
+++ src/main/java/pl/wsb/lab/Clinic.java
@@ -1,13 +1,14 @@
 package pl.wsb.lab;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Clinic {
 
     private List<Patient> patients;
 
-    public Patient createPatientProfile(String firstName, String lastName) {
-        Patient newPatient = new Patient(firstName, lastName);
+    public Patient createPatientProfile(String firstName, String lastName, String pesel, LocalDate birthDate, String phoneNumber, String eMail) {
+        Patient newPatient = new Patient(firstName, lastName, pesel, birthDate, phoneNumber, eMail);
         this.patients.add(newPatient);
         return newPatient;
     }
--- src/main/java/pl/wsb/lab/Patient.java
+++ src/main/java/pl/wsb/lab/Patient.java
@@ -1,17 +1,25 @@
 package pl.wsb.lab;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Patient {
 
     private String firstName;
     private String lastName;
+    private String pesel;
+    private LocalDate birthDate; // YYYY-MM-DD
+    private String phoneNumber;
+    private String eMail;
 
-
-    public Patient(String firsName, String lastName) {
+    public Patient(String firsName, String lastName, String pesel, LocalDate birthDate, String phoneNumber, String eMail) {
         this.firstName = firsName;
         this.lastName = lastName;
-
+        this.pesel = pesel;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.eMail = eMail;
     }
-
 
     public String getFirstName() {
         return firstName;
@@ -23,5 +31,25 @@ public class Patient {
 
     public String getFullName() {
         return firstName + " " + lastName;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public int calculateAge() {
+        return Period.between(birthDate, LocalDate.now()).getYears();
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEMail() {
+        return eMail;
     }
 }
