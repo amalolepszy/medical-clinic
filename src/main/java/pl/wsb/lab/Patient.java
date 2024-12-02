6d470694ad11bc6e40fd792e36573b58805ae976
amalolepszy <67199354+amalolepszy@users.noreply.github.com>
2024-12-02 20:09:54 +0100

    4: Przeszukiwanie pacjentow, refactor kodu, testy (#22)

--- src/main/java/pl/wsb/lab/Patient.java
+++ src/main/java/pl/wsb/lab/Patient.java
@@ -6,4 +6,6 @@ class Patient extends Person {
     public Patient(String firstName, String lastName, String pesel, LocalDate birthDate, String phoneNumber, String eMail) {
         super(firstName, lastName, pesel, birthDate, phoneNumber, eMail);
     }
+
+
 }
